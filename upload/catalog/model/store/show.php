abb9a6b657bd8e0bb136b45afaf43716ff7981d3
tangcheng <tangch@broadcom.com>
2015-12-20 14:41:34 +0800

    Fixed sql syntax error

--- upload/catalog/model/store/show.php
+++ upload/catalog/model/store/show.php
@@ -1,7 +1,7 @@
 <?php
 class ModelStoreShow extends Model {
 	public function getStoreInfo($storeId) {
-		$storeInfo = $this->db->query(" SELECT * FROME".DB_PREFIX."customer_store WHERE customer_store_id=".(int)$storeId);
+		$storeInfo = $this->db->query(" SELECT * FROM ".DB_PREFIX."customer_store WHERE customer_store_id=".(int)$storeId);
 
 		return $storeInfo;
 	}
