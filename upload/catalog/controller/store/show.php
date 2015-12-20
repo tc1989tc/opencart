ddcf9c64b29d3a5bfb55f310952d35c417239563
tangcheng <tangch@broadcom.com>
2015-12-20 14:45:30 +0800

    Modify template path

--- upload/catalog/controller/store/show.php
+++ upload/catalog/controller/store/show.php
@@ -21,7 +21,7 @@ class ControllerStoreShow extends Controller {
     $data['header'] = $this->load->controller('common/header');
 
         if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/store/show.tpl')) {			
-		$this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/common/home.tpl', $data));		
+		$this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/store/show.tpl', $data));		
 	} else {			
 		$this->response->setOutput($this->load->view('default/template/store/show.tpl', $data));	
 	}
