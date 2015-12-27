f0c568224276f25509642ea53719fbc385ad0636
tangcheng <tangch@broadcom.com>
2015-12-27 15:40:07 +0800

    aDD

--- upload/catalog/model/design/layout.php
+++ upload/catalog/model/design/layout.php
@@ -15,4 +15,38 @@ class ModelDesignLayout extends Model {
 		
 		return $query->rows;
 	}
+
+	public function getLayouts($data = array()) {
+		$sql = "SELECT * FROM " . DB_PREFIX . "layout";
+
+		$sort_data = array('name');
+
+		if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
+			$sql .= " ORDER BY " . $data['sort'];
+		} else {
+			$sql .= " ORDER BY name";
+		}
+
+		if (isset($data['order']) && ($data['order'] == 'DESC')) {
+			$sql .= " DESC";
+		} else {
+			$sql .= " ASC";
+		}
+
+		if (isset($data['start']) || isset($data['limit'])) {
+			if ($data['start'] < 0) {
+				$data['start'] = 0;
+			}
+
+			if ($data['limit'] < 1) {
+				$data['limit'] = 20;
+			}
+
+			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+		}
+
+		$query = $this->db->query($sql);
+
+		return $query->rows;
+	}
 }
